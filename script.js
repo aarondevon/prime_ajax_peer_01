071ae51181c9de5393a5d23045d5e413eaecdb92
Aaron Sawyer <me@aarondevon.com>
2015-04-30 16:55:46 -0500

    Creates json data and js functionality

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 /**
  * Created by aaronsawyer on 4/30/15.
  */
+var cityData = null;
+var cityHtml = null;
+var numCities = 0;
+var cityName, cityPop, cityArea, cityDensity;
+
+$(document).ready(function(){
+    $("#get-city-btn").on("click", function(){
+        if(numCities == 0) {
+            $.get('cities.html', function (data) {
+                $("#cityList").append(data);
+            });
+        } else if(numCities == 4) {
+            numCities = 0;
+            $("#cityListed").empty();
+        }
+
+        $.get('data.json', function(data) {
+            cityData = data;
+            cityName = cityData.cities[0].name;
+            cityPop = cityData.cities[0].population;
+            cityArea = cityData.cities[0].landArea;
+            cityDensity = cityData.cities[0].density;
+            $(".citiesListed").append("<div class='oneCity'><p>Name:</p></div>");
+
+            numCities++;
+        });
+
+
+    });
+
+});
